7c49d2db913e92bfb166794b398d7d7eac57f2e9
hubbu <ia@ivanarellano.com>
2011-12-03 23:35:17 -0500

    Create Experience object.

--- RollEmUp/src/com/tinyrender/rollemup/object/ObjectFactory.java
+++
@@ -1,8 +0,0 @@
-package com.tinyrender.rollemup.object;
-
-import com.badlogic.gdx.physics.box2d.World;
-import com.tinyrender.rollemup.GameObject;
-
-public interface ObjectFactory {
-	public abstract GameObject build(float x, float y, World world);
-}
