c6d323d843a7f7a1761e56cf7d9a5a926d241ecf
hubbu <ia@ivanarellano.com>
2012-01-25 14:50:40 -0800

    Variables renamed

--- RollEmUp/src/com/tinyrender/rollemup/RollEmUp.java
+++ RollEmUp/src/com/tinyrender/rollemup/RollEmUp.java
@@ -3,13 +3,14 @@ package com.tinyrender.rollemup;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.tinyrender.rollemup.gui.Gui;
 import com.tinyrender.rollemup.screen.SplashScreen;
 
 public class RollEmUp extends Game implements ApplicationListener {
 	public final static int TARGET_WIDTH = 854;
 	public final static int TARGET_HEIGHT = 480;
-	public final static int SCREEN_HALF_WIDTH = (int) Math.ceil(TARGET_WIDTH / 2);
-	public final static int SCREEN_HALF_HEIGHT = (int) Math.ceil(TARGET_HEIGHT /2);
+	public final static int TARGET_HALF_WIDTH = (int) Math.ceil(TARGET_WIDTH / 2);
+	public final static int TARGET_HALF_HEIGHT = (int) Math.ceil(TARGET_HEIGHT /2);
 	public boolean finishedLoading = false;
 	public ScreenStack screenStack;
 	public Gui gui;
@@ -26,6 +27,8 @@ public class RollEmUp extends Game implements ApplicationListener {
 		Gdx.graphics.setVSync(true);
 		Gdx.input.setCatchBackKey(true);
 		Gdx.input.setCatchMenuKey(true);
+		
+		//Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		
 		gui = new Gui();
 	}
